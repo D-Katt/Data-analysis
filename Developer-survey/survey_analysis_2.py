a70e3f769ce49471eb1ed1a2268d20689b1bf424
D-Katt <58660996+D-Katt@users.noreply.github.com>
2020-01-25 15:46:08 +0300

    Analysis using pandas and numpy

--- Developer-survey/survey_analysis_2.py
+++ Developer-survey/survey_analysis_2.py
@@ -5,8 +5,9 @@
 import numpy as np
 import pandas as pd
 
-# Считываем данные из csv-файла:
-survey = pd.read_csv('survey_results_public.csv')
+# Считываем из csv-файла данные, необходимые для анализа:
+survey = pd.read_csv('survey_results_public.csv', usecols=['Respondent', 'Country',
+                     'ConvertedComp', 'EdLevel', 'Employment', 'YearsCodePro'])
 
 # Общее количество респондентов и количество вопросов
 
@@ -17,23 +18,24 @@ print("Анализ 'Stack Overflow Developer Survey 2019':\n"
 
 # Распределение респондентов по странам
 
-countries = survey[['Respondent', 'Country']]  # Выбираем столбцы: порядковый номер и страна
-countries_n = countries.groupby('Country').count()  # Кол-во респондентов по странам
-countries_total = survey.Country.count()  # Всего респондентов, указавших страну
-countries_n['Share'] = round(countries_n['Respondent'] / countries_total * 100, 2)  # Добавляем столбец с долей
-countries_n.sort_values('Respondent', ascending=False, inplace=True)  # Сортируем по убыванию
-
+# Подсчитываем кол-во по странам и из 20-ки крупнейших создаем новую таблицу:
+countries = pd.DataFrame(survey.Country.value_counts().nlargest(20))
+countries.columns = ['N_of_Developers']  # Переименовываем столбец
+total = survey.Country.count()  # Всего респондентов, указавших страну
+# Добавляем столбец с долей от общего числа разработчиков:
+countries['Percentage'] = round(countries['N_of_Developers'] / total * 100, 2)
 print('\nТоп-20 стран по количеству разработчиков:')
-print(countries_n.head(20))  # Выводим первые 20 строк
+print(countries)
 
 # Уровень дохода
 
-av_income = int(np.mean(survey['ConvertedComp']))
+av_income = int(np.mean(survey['ConvertedComp']))  # Среднее значение по столбцу
 print('\nСредний уровень дохода, долл. США в год:', av_income)
 
 # Средний доход по странам
 
-income_country = survey[['ConvertedComp', 'Country']].groupby('Country').mean().round(0)  # Среднее значение
+# Среднее значение с группировкой по странам:
+income_country = survey[['ConvertedComp', 'Country']].groupby('Country').mean().round(0)
 income_country.sort_values('ConvertedComp', ascending=False, inplace=True)  # Сортировка по убыванию
 print('\nСредний уровень дохода по странам:')
 print(income_country.head(20))
@@ -54,7 +56,22 @@ print(income_employment)
 
 # Средний доход в зависимости от опыта работы
 
-income_experience = survey[['ConvertedComp', 'YearsCodePro']].groupby('YearsCodePro').mean().round(0)
-income_experience.sort_values('ConvertedComp', ascending=False, inplace=True)
+# Заменяем исходные текстовые значения в столбце числовыми:
+for i in range(len(survey)):
+    if survey.loc[i, 'YearsCodePro'] == 'Less than 1 year':
+        survey.loc[i, 'YearsCodePro'] = '0.5'
+    elif survey.loc[i, 'YearsCodePro'] == 'More than 50 years':
+        survey.loc[i, 'YearsCodePro'] = 51
+
+# Преобразуем данные столбца в числовой формат:
+survey.YearsCodePro = survey.YearsCodePro.apply(pd.to_numeric, errors='coerce')
+
+# Преобразуем числовые данные столбца в укрупненные категории:
+survey.YearsCodePro = pd.cut(survey.YearsCodePro, bins=[0, 1, 3, 6, 11, 21, 50],
+                           labels=['Less than 1 year', '1 to 2 years', '3 to 5 years',
+                                   '6 to 10 years', '11 to 20 years', 'Over 20 years'])
+
+# Рассчитываем средние значения по категориям:
+income_experience = survey[['YearsCodePro', 'ConvertedComp']].groupby('YearsCodePro').mean().round(0)
 print('\nСредний доход в зависимости от опыта работы:')
-print(income_experience.head(20))
+print(income_experience)
