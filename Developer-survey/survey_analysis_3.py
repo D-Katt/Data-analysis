6a3c8ba607ba0d97df1748082ec0a36cc55b9554
D-Katt <58660996+D-Katt@users.noreply.github.com>
2020-01-29 16:51:47 +0300

    Analysis using pandas and matplotlib

--- Developer-survey/survey_analysis_3.py
+++ Developer-survey/survey_analysis_3.py
@@ -19,14 +19,15 @@ income_experience_p = survey[survey.LanguageWorkedWith.str.contains('Python', na
 income_experience_p = income_experience_p[['YearsCodePro', 'ConvertedComp']].groupby('YearsCodePro').mean().round(0)
 
 plt.style.use('fivethirtyeight')
-plt.plot(income_experience_p.index, income_experience_p.ConvertedComp, label='Python Developers')
-plt.plot(income_experience.index, income_experience.ConvertedComp, label='All Developers')
-plt.title('Average Developer Salaries by Age')
-plt.legend()
-plt.xlabel('Ages')
-plt.ylabel('Annual Salary (USD)')
-plt.tight_layout()
-plt.show()
+
+fig1, ax1 = plt.subplots()
+
+ax1.plot(income_experience_p.index, income_experience_p.ConvertedComp, label='Python Developers')
+ax1.plot(income_experience.index, income_experience.ConvertedComp, label='All Developers')
+ax1.set_title('Average Income by Coding Experience')
+ax1.legend()
+ax1.set_xlabel('Years')
+ax1.set_ylabel('Annual Income (USD)')
 
 # Наиболее популярные языки программирования:
 lang_response = survey['LanguageWorkedWith']
@@ -42,22 +43,24 @@ lang_response.sort_values(by='Developers_N', ascending=False, inplace=True)
 total = survey['LanguageWorkedWith'].count()
 lang_response['Percentage'] = round(lang_response.Developers_N / total * 100, 2)
 
-fig, ax = plt.subplots()
-rows = ax.barh(lang_response.head(10).index, lang_response.head(10).Percentage)
-ax.set_title("Usage of Programming Languages (%)")
+fig2, ax2 = plt.subplots()
+rows = ax2.barh(lang_response.head(10).index, lang_response.head(10).Percentage)
+ax2.set_title("Usage of Programming Languages (%)")
 
 
-def bar_labels(bars):
-    """Attach a text label to each bar in 'bars', displaying its value."""
-    for rect in bars:
-        width = rect.get_width()
+def bar_labels(ax, rows):
+    """Attach a text label to each row in 'rows', displaying its value."""
+    for row in rows:
+        width = row.get_width()
         ax.annotate('{}%'.format(width),
-                    xy=(width, rect.get_y()),
+                    xy=(width, row.get_y()),
                     xytext=(0, 10),
                     textcoords="offset points",
                     ha='right', va='center')
 
 
-bar_labels(rows)
-fig.tight_layout()
+bar_labels(ax2, rows)
+
+fig1.tight_layout()
+fig2.tight_layout()
 plt.show()
